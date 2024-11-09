766defb8bed676f487770594852041136d57f141
Rihab76-coder <rihabachour2000257@gmail.com>
2024-11-09 12:17:39 +0100

    Create list .py

---
+++ list .py
@@ -0,0 +1,7 @@
+# List
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # Accessing the first element
+
+# Tuple
+colors = ("red", "green", "blue")
+print(colors[1])  # Accessing the second element
